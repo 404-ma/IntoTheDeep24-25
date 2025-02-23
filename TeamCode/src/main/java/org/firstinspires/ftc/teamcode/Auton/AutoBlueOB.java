9b01ede8d2512f7cd61a1e234bcccef4d06af9f6
jaltrichter <jaltrichter@outlook.com>
2025-02-23 03:19:58 -0500

    Disable Unused

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/AutoBlueOB.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/AutoBlueOB.java
@@ -24,7 +24,7 @@ public class AutoBlueOB extends LinearOpMode {
 
     public static class Params {
         public boolean easy = false;
-        public String version = "15.14";
+        public String version = "16.0";
         public double y = 38.4;
     }
 
