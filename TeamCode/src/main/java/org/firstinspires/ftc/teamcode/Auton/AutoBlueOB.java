a25b4f985848b6f6560f11389f87581773db0326
asvika425 <148814963+asvika425@users.noreply.github.com>
2025-02-23 02:47:09 -0500

    auton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/AutoBlueOB.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/AutoBlueOB.java
@@ -130,7 +130,7 @@ public class AutoBlueOB extends LinearOpMode {
         // Drive to Wall and Dump
         Action PickupTurn = drive.actionBuilder(drive.pose)
                 .setReversed(true)
-                .splineTo(new Vector2d(5, 28), 0)
+                .splineTo(new Vector2d(3.5, 28), 0)
                 .build();
         Actions.runBlocking(new ParallelAction(PickupTurn, Beak.autonPickupToSlide()));
         Actions.runBlocking(new SequentialAction(Beak.autonDropToHuman(), Claw.grabFromHuman(), new ParallelAction(Viper.fast_perfBeforeDropOff(), Bucket.autonBucketDown())));
