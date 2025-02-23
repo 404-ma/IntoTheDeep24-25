05b108d5ff879f28d838e05dd80a9c953136de6e
jaltrichter <jaltrichter@outlook.com>
2025-02-23 02:37:16 -0500

    Version

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/AutoBlueOB.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/AutoBlueOB.java
@@ -24,7 +24,7 @@ public class AutoBlueOB extends LinearOpMode {
 
     public static class Params {
         public boolean easy = false;
-        public String version = "15.9";
+        public String version = "15.10";
         public double y = 38.4;
     }
 
